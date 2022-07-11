53d7e5632c06afadef9a11ef61198663e1eb0265
StellahMbao <woodzdj@gmail.com>
2022-07-11 05:13:42 -0700

    Examples

---
+++ 0x05-pointers_arrays_strings/examples/10-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * main - you can not modify a
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
---
+++ 0x05-pointers_arrays_strings/examples/4-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - storing the address of the variable into a pointer
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+	int *p;
+
+	n = 98;
+	p = &n;
+
+	printf("Address of 'n': %p\n", &n);
+	printf("Value of 'p': %p\n", p);
+	return (0);
+}
+
