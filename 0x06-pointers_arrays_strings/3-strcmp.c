3c5ebbfa823a62a6dc836108e180b3fff798c049
StellahMbao <woodzdj@gmail.com>
2022-07-15 00:43:03 -0700

    Bug fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,10 +6,16 @@
  * _strcmp - compares 2 strings
  * @s1: first string
  * @s2: second string
+ * Return: nothing
  */
 int _strcmp(char *s1, char *s2)
 {
-	int compare = strcmp(s1, s2);
-
-	return (compare);
+	while (*s1 != '\0' || *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	return (0);
 }
