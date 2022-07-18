709db4807692da2ad24d3e4133fbe7ab7baee3c6
StellahMbao <woodzdj@gmail.com>
2022-07-18 11:08:03 -0700

    Answer files

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,10 +3,27 @@
 #include <string.h>
 
 /**
- * _strstr - ocates a substring
- * @haystack:
- * @needle:
+ * _strstr - locates a substring
+ * @haystack:string
+ * @needle:substring
+ * Return: Pointer t beginning of string or null
  */
 char *_strstr(char *haystack, char *needle)
 {
+	while (*haystack != '\0')
+	{
+		char *beginning = haystack;
+		char *pattern = needle;
+
+		while (*pattern == *haystack && *pattern != '\0'
+				&& *haystack != '\0')
+		{
+			haystack++;
+			pattern++;
+		}
+		if (*pattern == '\0')
+			return (beginning);
+		haystack = beginning + 1;
+	}
+	return ('\0');
 }
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,10 +3,27 @@
 #include <string.h>
 
 /**
- * print_diagsums - prints the sum of the two diagonals of a square matrix of integers
- * @a:
- * @size:
+ * print_diagsums - prints the sum of the two diagonals of
+ * a square matrix of integers
+ * @a: matrix
+ * @size:size of matrix
  */
 void print_diagsums(int *a, int size)
 {
+	int diagonal_1 = 0;
+	int diagonal_2 = 0;
+	int row, i;
+
+	for (row = 0; row < size; row++)
+	{
+		i = (row * size) + row;
+		diagonal_1 += a[i];
+	}
+	for (row = 1; row <= size; row++)
+	{
+		i = (row * size) - row;
+		diagonal_2 += a[i];
+	}
+
+	printf("%d, %d\n", diagonal_sum_1, diagonal_sum_2);
 }
