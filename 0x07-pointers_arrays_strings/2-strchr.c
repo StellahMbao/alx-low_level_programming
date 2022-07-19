2e077794af3f7055be07803b6f4885777284b990
StellahMbao <woodzdj@gmail.com>
2022-07-19 00:20:58 -0700

    New updates

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,13 +13,10 @@ char *_strchr(char *s, char c)
 	int index = 0;
 
 	while (s[index] != '\0' && s[index] != c)
-	
 		index++;
-	
 	if (s[index] == c)
-		return (&s[index]); 
-	
+		return (&s[index]);
 	else
 		return (NULL);
-	
+
 }
