0dcee5dd9ddab7b0ada8210151716a5780af67fc
StellahMbao <woodzdj@gmail.com>
2022-09-30 10:58:11 -0700

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,12 +2,61 @@
 #include <stdlib.h>
 
 /**
+ * insert_node - insert node at given index
+ * @tmp: ptr to nth position node in doubly linked list
+ *  @n: node data
+ *  Return: address of inserted node
+ */
+dlistint_t *insert_node(dlistint_t *tmp, int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(struct dlistint_s));
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->n = n;
+
+	new->next = tmp;
+	new->prev = tmp->prev;
+	tmp->prev->next = new;
+	tmp->prev = new;
+	return (new);
+}
+
+/**
  * insert_dnodeint_at_index - inserts new node at given position
  * @h: poinetr to head pointer
- * @idx:
- * @n:
+ * @idx: node index
+ * @n: node data
  * Return: Address of new node || NULL
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *tmp;
+
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+	if (!h)
+		return (NULL);
+
+	tmp = *h;
+	while ((idx != 0) && (tmp->next))
+	{
+		idx -= 1;
+		tmp = tmp->next;
+		if (idx == 0)
+		{
+			return (insert_node(tmp, n));
+		}
+	}
+	if (idx == 1)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	return (NULL);
+}
 
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - function deletes node at
+ * given inde
+ * @head: head pointer of list
+ * @index: index
+ * Return: 1 for success || -1 for failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+
